67d449d07321a11bade106be62ffabd9a100455b
Sheris-Milly <157208725+Sheris-Milly@users.noreply.github.com>
2024-11-02 23:52:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def ML_page():
     return render_template('ML.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
 
 
 #imad
